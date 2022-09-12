44ad75d36147d07229326b855768c9fb7a19b575
Jasonchen-66 <jianbang@ualberta.ca>
2022-09-12 11:40:33 -0600

    mern app finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import HomeComponent from "./components/home-component";
+import NavComponent from "./components/nav-component";
+import RegisterComponent from "./components/register-component";
+import LoginComponent from "./components/login-component";
+import ProfileComponent from "./components/profile-component";
+import CourseComponent from "./components/course-component";
+import PostCourseComponent from "./components/postCourse-component";
+import EnrollComponent from "./components/enroll-component";
+import AuthService from "./services/auth.service";
 
 function App() {
+  let [currentUser, setCurrentUser] = useState(AuthService.getCurrentUser());
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavComponent currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      <Routes>
+        <Route path="/" element={<HomeComponent />}/>
+        <Route path="/register" element={<RegisterComponent />}/>
+        <Route path="/login" element={<LoginComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />}/>
+        <Route path="/profile" element={<ProfileComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />}/>
+        <Route path="/course" element={<CourseComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />}/>
+        <Route path="/postCourse" element={<PostCourseComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />}/>
+        <Route path="/enroll" element={<EnrollComponent
+            currentUser={currentUser}
+            setCurrentUser={setCurrentUser}
+          />}/>
+      </Routes>
     </div>
   );
 }
